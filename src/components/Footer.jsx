4e732ed1fb6c39a8f1b094952d9e43811a98fd20
emrecoban <fbdarktole@gmail.com>
2023-03-21 16:44:47 +0300

    Added github url and meta tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     return (
         <div id="footer" className="bg-white shadow p-3 absolute inset-x-0 bottom-0 font-[Inter] text-sm">
             <footer className="max-w-2xl w-full mx-auto text-center">
-                &copy; 2023 <span className="text-green-600">eksiShot</span> built by emrecoban with ReactJS · <a href="#" className="text-blue-500 hover:text-blue-600">GitHub</a>
+                &copy; 2023 <span className="text-green-600">eksiShot</span> built by emrecoban with ReactJS · <a href="https://github.com/emrecoban/eksiShot" className="text-blue-500 hover:text-blue-600" target="_blank">GitHub</a>
             </footer>
         </div>
     )
