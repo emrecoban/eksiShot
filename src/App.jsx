63b4f47a4d1f54eac90b21a5e358829d2538f156
emrecoban <fbdarktole@gmail.com>
2023-04-12 22:08:42 +0300

    Hope last fix for Analytics!

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import EntryContextProvider from "./provider/EntryContext";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
 import Footer from "./components/Footer";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
-export default function App(){
-    return (
-        <EntryContextProvider>
-            <Header />
-            <Entry />
-            <Footer />
-            <Analytics />
-        </EntryContextProvider>
-    )
+export default function App() {
+  return (
+    <EntryContextProvider>
+      <Header />
+      <Entry />
+      <Footer />
+      <Analytics />
+    </EntryContextProvider>
+  );
 }
