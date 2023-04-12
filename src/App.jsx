61cc3a5df2e647451688f2b045dcebbe68026278
emrecoban <fbdarktole@gmail.com>
2023-04-12 21:37:00 +0300

    Added @vercel/analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import EntryContextProvider from "./provider/EntryContext";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
 import Footer from "./components/Footer";
+import { inject } from '@vercel/analytics';
 
 export default function App(){
+    inject();
     return (
         <EntryContextProvider>
             <Header />
